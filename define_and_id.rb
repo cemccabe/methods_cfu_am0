3d029d9cf539e7e3f68db29c6aee4096249457b5
Christian McCabe <christian.mccabe89@gmail.com>
2022-09-15 11:42:33 -0600

    Complete define_and_id exercise

--- define_and_id.rb
+++ define_and_id.rb
@@ -8,20 +8,27 @@
 "Hello World".downcase
 
 # YOU DO the rest:
-
-
+# The include? method is called on the string object "Hello World"
+# The argument "Hello" is passed; include? will return a boolean value if the string contains the given string or character
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "Hello" is passed; end_with? will return true if the string ends with the suffix passed
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "Hello" is passed; end_with? will return true if the string ends with the suffix passed
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer object 12
+# No arguments are passed; even? will true if the integer is an even number
+# The return value is true
 12.even?
 
-
+# The next method is called on the integer object 18
+# No arguments are passed; next will return the next number (i.e. int + 1)
+# The return value is 19
 18.next
-
-
